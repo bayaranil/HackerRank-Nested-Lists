9bee3815fefd64aef52fbfcfa64f8adfda69bdd5
Anıl Bayar <75955104+bayaranil@users.noreply.github.com>
2022-12-07 17:57:35 +0300

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -1,13 +1,28 @@
-name_List =[]
-score_List =[]
+if __name__ == '__main__':
+    name_List =[]
+    score_List =[]
+    my_List = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        name_List.append(name)
+        score_List.append(score) 
+    score_List.sort()
+    name_List.sort()
+    print (name_List)
+    print (score_List)
+    for _ in score_List:
+        if _ == int(max(score_List)):
+            score_List.remove(int(max(score_List)))
+    for _ in score_List:
+        if _ == score_List[0]:
+            my_List.append(_)
+            print (_)
+        else:
+            my_List.append(name_List[0])
+            print (name_List[1])
+        
+  
+ 
+  
 
-for _ in range(int(input())):
-  name= input ()
-  score = float(input ())
-  name_List.append(name)
-  score_List.append(score)
-
-score_List.sort()
-name_List.sort()
-print (name_List)
-print (score_List)
